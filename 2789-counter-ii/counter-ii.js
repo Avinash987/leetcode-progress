e0ae4a21f54efe7d5e903de19a4d121fb72c2ebc
Lalith Avinash Donkina <lalithd1@umbc.edu>
2026-02-20 11:03:15 -0500

    Time: 67 ms (5.70%) | Memory: 56.7 MB (46.18%) - LeetSync

--- 2789-counter-ii/counter-ii.js
+++ 2789-counter-ii/counter-ii.js
@@ -3,27 +3,22 @@
  * @return { increment: Function, decrement: Function, reset: Function }
  */
 var createCounter = function(init) {
-
-    const obj = {
-        orig_value: init,
-        curr_value: init,
-        increment: function() {
-            this.curr_value += 1;
-            return this.curr_value;
-        },
-        decrement: function() {
-            this.curr_value  -= 1;
-            return this.curr_value;
-        },
-        reset: function() {
-            this.curr_value = this.orig_value;
-            return this.curr_value;
-        }
-    };
-    return obj;
-    
+  let currentCount = init;
+  return {
+    increment: function() {
+       currentCount += 1;
+       return currentCount;
+    },
+    decrement: function() {
+       currentCount -= 1;
+       return currentCount;
+    },
+    reset: function() {
+       currentCount = init;
+       return currentCount;
+    },
+  }
 };
-
 /**
  * const counter = createCounter(5)
  * counter.increment(); // 6
